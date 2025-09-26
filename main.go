8d0f057e962a33e8a86b6a9ab598360936b477bc
Flurin Brühwiler <flurin.bruehwiler@cmiag.ch>
2025-09-26 15:51:09 +0200

    added raylib

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import rl "github.com/gen2brain/raylib-go/raylib"
 import "fmt"
 
 func main() {
-	fmt.Println("ok")
+
+	fmt.Println("Hi")
+
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.Black)
+
+		rl.EndDrawing()
+	}
 }
